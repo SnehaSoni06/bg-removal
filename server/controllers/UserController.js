317d80a02877916cedfbe679e664ed129b18d11f
SnehaSoni06 <snehasonivns@gmail.com>
2024-11-12 03:41:17 +0530

    webhooks added

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,84 @@
+import { Webhook } from 'svix';
 
+// async function initializeWebhook() {
+//   const base64 = await import('@stablelib/base64');
+//   // Now you can use `base64` here.
+// }
+
+// initializeWebhook().catch(console.error);
+
+import userModel from '../models/userModel.js'
 
 //API Controller function to manage clerk user with database
 // http://localhost:4000/api/user/webhooks
 const clerkWebhooks = async (req,res)=>{
-    
+
+    try{
+
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        // const { base64 } = await import('@stablelib/base64');
+
+        await whook.verify(JSON.stringify(req.body),{
+            "svix-id":req.headers["svix-id"],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"]
+        })
+
+        const {data, type}=req.body
+
+        switch (type) {
+            case "user.created":{
+
+                const userData={
+                    clerkId:data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstName:data.first_name,
+                    lastName:data.last_name,
+                    photo: data.image_url
+                }
+
+                await userModel.create(userData)
+                res.json({})
+               
+                break;
+            }
+
+            case "user.updated":{
+
+                const userData={
+                    email: data.email_addresses[0].email_address,
+                    firstName:data.first_name,
+                    lastName:data.last_name,
+                    photo: data.image_url
+                }
+
+                await userModel.findOneAndUpdate({clerkId:data.id},userData)
+                res.json({})
+
+                break;
+            }
+
+            case "user.deleted":{
+
+                await userModel.findOneAndDelete({clerkId:data.id})
+                res.json({})
+               
+                break;
+            }
+                
+               
+        
+            default:
+                break;
+        }
+
+
+
+    }catch(error){
+        console.log(error.message)
+        res.json({success:false,message:error.message})
+        
+    }
+
 }
+export {clerkWebhooks}
